331f5a7f8cc1d2c020a0526587769784da30c661
SillySamShapiro <sasha001@mail.goucher.edu>
2017-07-18 20:41:37 -0400

    LOTS of pictures

--- SamStuff/Databoxmaker.m
+++ SamStuff/Databoxmaker.m
@@ -1,17 +1,59 @@
 %Creates a 3d array of 24x24 integrated images (x), with a vector (y)
 %where y(i) = 1 if x(:,:,i) is a face, and y(i)= 0 if x(:,:,i) is not a face.
 %The file paths must be changed if you are going to use this on a different computer!
-xtest = zeros(24,24,100);
+x2 = zeros(24,24,200);
 
 
-for i = 1:50
-   xtest(:,:,i) = csvread(strcat('../images/Converted Images/Positive_converted/Face_',num2str(i+150),'_integral.dat')); 
+for i = 1:200
+   x2(:,:,i) = csvread(strcat('../images/Converted Images/Negative_converted/neg_',num2str(i),'_integral.dat')); 
 end
 
-for i = 1:50
-    xtest(:,:,i+50) = csvread(strcat('../images/Converted Images/Negative_converted/UMD_',num2str(i+50),'_integral.dat'));
-end
 
-ytest = zeros(1,100);
 
-ytest(1:50) = 1;
+
+
+% for i = 1:50
+%     xtest(:,:,i+50) = csvread(strcat('../images/Converted Images/Negative_converted/UMD_',num2str(i+50),'_integral.dat'));
+% end
+
+y2 = zeros(1,200);
+
+% ytest(1:50) = 1;
+
+
+%For the Yale faces:
+% for i = 1:10
+%    x2(:,:,i+10) = csvread(strcat('../images/Converted Images/Yale Faces Converted/YaleFaceGlasses_',num2str(i),'_int.dat')); 
+% end
+% 
+% for i = 1:10
+%    x2(:,:,i+20) = csvread(strcat('../images/Converted Images/Yale Faces Converted/YaleFaceHappy_',num2str(i),'_int.dat')); 
+% end
+% 
+% for i = 1:10
+%    x2(:,:,i+30) = csvread(strcat('../images/Converted Images/Yale Faces Converted/YaleFaceleftlight_',num2str(i),'_int.dat')); 
+% end
+% 
+% for i = 1:10
+%    x2(:,:,i+40) = csvread(strcat('../images/Converted Images/Yale Faces Converted/YaleFaceNoGlasses_',num2str(i),'_int.dat')); 
+% end
+% 
+% for i = 1:10
+%    x2(:,:,i+50) = csvread(strcat('../images/Converted Images/Yale Faces Converted/YaleFaceNormal_',num2str(i),'_int.dat')); 
+% end
+% 
+% for i = 1:10
+%    x2(:,:,i+60) = csvread(strcat('../images/Converted Images/Yale Faces Converted/YaleFaceRightLight_',num2str(i),'_int.dat')); 
+% end
+% 
+% for i = 1:10
+%    x2(:,:,i+70) = csvread(strcat('../images/Converted Images/Yale Faces Converted/YaleFaceSad_',num2str(i),'_int.dat')); 
+% end
+% 
+% for i = 1:10
+%    x2(:,:,i+80) = csvread(strcat('../images/Converted Images/Yale Faces Converted/YaleFaceSleepy_',num2str(i),'_int.dat')); 
+% end
+% 
+% for i = 1:10
+%    x2(:,:,i) = csvread(strcat('../images/Converted Images/Yale Faces Converted/YaleFaceSurprised_',num2str(i),'_int.dat')); 
+% end
--- SamStuff/ImportMultipleImages.m
+++ SamStuff/ImportMultipleImages.m
@@ -4,15 +4,23 @@ y = zeros(24,24);
 
 
 
-for i = 1:6
+for i = 1:9
     
-y = imresize(imread(strcat('../images/Internet Pictures/Faces/InternetFace',num2str(i),'.jpg')),[24,24]);
+y = imresize(imread(strcat('../images/Yale Faces/subject0',num2str(i),'.surprised.pgm')),[24,24]);
 
 y = integralimage(y);
 
-csvwrite(strcat('../images/Converted Images/Positive_converted/Internet Faces/InternetFace_',num2str(i),'_integral.dat'),y);
+csvwrite(strcat('../images/Converted Images/Yale Faces converted/YaleFaceSurprised_',num2str(i),'_int.dat'),y);
 end
 
+for i = 10:15
+    
+y = imresize(imread(strcat('../images/Yale Faces/subject',num2str(i),'.surprised.pgm')),[24,24]);
+
+y = integralimage(y);
+
+csvwrite(strcat('../images/Converted Images/Yale Faces converted/YaleFaceSurprised_',num2str(i),'_int.dat'),y);
+end
 % I Figured out how to solve the problems described below.
         %%Notes/Ideas:
         %The only way to make a string out of a vector is with num2str(x(i)) for the individual entries
